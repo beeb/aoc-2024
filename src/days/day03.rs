dfa1ca9a325f37a5c0385735964ce5c4401a1956
beeb <703631+beeb@users.noreply.github.com>
2024-12-03 06:52:37 +0100

    feat: day 3
    feat(day03): part 1
    
    feat: day03
    
    refactor(day03): use parser
    
    docs: comments
    
    docs: add regex
    
    refactor(day03): remove type

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,25 +1,96 @@
-use winnow::PResult;
+use winnow::{
+    ascii::digit1,
+    combinator::{alt, delimited, repeat, repeat_till, separated_pair, terminated},
+    token::any,
+    PResult, Parser as _,
+};
 
 use crate::days::Day;
 
 pub struct Day03;
 
+/// An instruction, either "mul", "do" or "don't"
+#[derive(Debug)]
+pub enum Instr {
+    Mul { x: u64, y: u64 },
+    Do,
+    Dont,
+}
+
+/// Parser for multiplication instructions
+fn parse_mul(input: &mut &str) -> PResult<Instr> {
+    let (x, y) = delimited(
+        "mul(",
+        separated_pair(digit1.parse_to(), ',', digit1.parse_to()),
+        ')',
+    )
+    .parse_next(input)?;
+    Ok(Instr::Mul { x, y })
+}
+
+/// Parser for "do" instructions
+fn parse_do(input: &mut &str) -> PResult<Instr> {
+    "do()".map(|_| Instr::Do).parse_next(input)
+}
+
+/// Parser for "don't" instructions
+fn parse_dont(input: &mut &str) -> PResult<Instr> {
+    "don't()".map(|_| Instr::Dont).parse_next(input)
+}
+
+/// Parser for a sequence of garbage bytes followed by an instruction
+///
+/// This parser consumes but ignores the garbage portion.
+fn parse_instr(input: &mut &str) -> PResult<Instr> {
+    let ((), instr) =
+        repeat_till(0.., any, alt((parse_mul, parse_do, parse_dont))).parse_next(input)?;
+    Ok(instr)
+}
+
 impl Day for Day03 {
-    type Input = String;
+    type Input = Vec<Instr>;
 
-    fn parser(_input: &mut &str) -> PResult<Self::Input> {
-        unimplemented!("parser")
+    /// Parsing took 119.5us
+    ///
+    /// Could also be done with a regex: (?:mul\((\d+),(\d+)\)|(do(?:n't)?\(\)).*?)+?
+    fn parser(input: &mut &str) -> PResult<Self::Input> {
+        terminated(
+            repeat(1.., parse_instr),
+            repeat::<_, _, (), _, _>(0.., any), // there could be garbage after the last instruction
+        )
+        .parse_next(input)
     }
 
-    type Output1 = usize;
+    type Output1 = u64;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    /// Part 1 took 1.1us
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        input
+            .iter()
+            .filter_map(|m| match m {
+                Instr::Mul { x, y } => Some(x * y),
+                _ => None,
+            })
+            .sum()
     }
 
-    type Output2 = usize;
+    type Output2 = u64;
 
-    fn part_2(_input: &Self::Input) -> Self::Output2 {
-        unimplemented!("part_2")
+    /// Part 2 took 2.67us
+    fn part_2(input: &Self::Input) -> Self::Output2 {
+        let mut accumulate = true;
+        let mut sum = 0;
+        for instr in input {
+            match instr {
+                Instr::Mul { x, y } => {
+                    if accumulate {
+                        sum += x * y
+                    }
+                }
+                Instr::Do => accumulate = true,
+                Instr::Dont => accumulate = false,
+            }
+        }
+        sum
     }
 }
