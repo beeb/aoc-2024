442f2e214e57a7a897bc7f110e871b09c621358f
beeb <703631+beeb@users.noreply.github.com>
2024-12-04 08:00:57 +0100

    feat(day04): part 1

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,20 +1,86 @@
-use winnow::PResult;
+use winnow::{
+    ascii::line_ending,
+    combinator::{repeat, separated},
+    token::one_of,
+    PResult, Parser as _,
+};
 
 use crate::days::Day;
 
+const GRID_SIZE: isize = 140;
+const WORD: [char; 4] = ['X', 'M', 'A', 'S'];
+const DIRS: [[isize; 2]; 8] = [
+    [0, -1],  // up
+    [1, -1],  // top right
+    [1, 0],   // right
+    [1, 1],   // bottom right
+    [0, 1],   // down
+    [-1, 1],  // bottom left
+    [-1, 0],  // left
+    [-1, -1], // top left
+];
+
 pub struct Day04;
 
-impl Day for Day04 {
-    type Input = String;
+#[derive(Debug)]
+pub struct Row {
+    cells: Vec<char>,
+}
 
-    fn parser(_input: &mut &str) -> PResult<Self::Input> {
-        unimplemented!("parser")
+#[derive(Debug)]
+pub struct Grid {
+    rows: Vec<Row>,
+}
+
+impl Grid {
+    fn is_letter(&self, x: isize, y: isize, letter: &char) -> Option<bool> {
+        if x < 0 || y < 0 {
+            return None;
+        }
+        self.rows
+            .get(y as usize)
+            .and_then(|row| row.cells.get(x as usize))
+            .map(|cell| cell == letter)
+    }
+
+    fn search_dir(&self, col: isize, row: isize, dir: &[isize; 2]) -> bool {
+        for (i, c) in WORD.iter().enumerate().skip(1) {
+            let x = col + dir[0] * i as isize;
+            let y = row + dir[1] * i as isize;
+            if let Some(true) = self.is_letter(x, y, c) {
+                continue;
+            }
+            return false;
+        }
+        true
+    }
+}
+
+fn parse_row(input: &mut &str) -> PResult<Row> {
+    let letters = repeat(1.., one_of('A'..='z')).parse_next(input)?;
+    Ok(Row { cells: letters })
+}
+
+impl Day for Day04 {
+    type Input = Grid;
+
+    fn parser(input: &mut &str) -> PResult<Self::Input> {
+        let rows = separated(1.., parse_row, line_ending).parse_next(input)?;
+        Ok(Grid { rows })
     }
 
     type Output1 = usize;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    fn part_1(grid: &Self::Input) -> Self::Output1 {
+        let mut count = 0;
+        for y in 0..GRID_SIZE {
+            for x in 0..GRID_SIZE {
+                if let Some(true) = grid.is_letter(x, y, &WORD[0]) {
+                    count += DIRS.iter().filter(|dir| grid.search_dir(x, y, dir)).count();
+                }
+            }
+        }
+        count
     }
 
     type Output2 = usize;
