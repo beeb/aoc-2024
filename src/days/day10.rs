26d74704bbbf0b2637fe39ccc45928e46d980fa8
beeb <703631+beeb@users.noreply.github.com>
2024-12-10 06:50:37 +0100

    wip: day 10

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,20 +1,69 @@
-use winnow::PResult;
+use pathfinding::grid::Grid;
+use winnow::{
+    ascii::line_ending,
+    combinator::{repeat, separated},
+    token::none_of,
+    PResult, Parser as _,
+};
 
 use crate::days::Day;
 
+const GRID_SIZE: usize = 48;
+
+/// Top - Right - Bottom - Left
+const DIRS: [(isize, isize); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+
 pub struct Day10;
 
-impl Day for Day10 {
-    type Input = String;
+#[derive(Debug, PartialEq, Eq, Default, Clone, Hash)]
+pub struct Point {
+    x: i8,
+    y: i8,
+}
 
-    fn parser(_input: &mut &str) -> PResult<Self::Input> {
-        unimplemented!("parser")
+#[derive(Debug, Clone, Default)]
+pub struct Puzzle {
+    map: Vec<Vec<u8>>,
+    trail_heads: Vec<Point>,
+}
+
+fn parse_row(input: &mut &str) -> PResult<Vec<u8>> {
+    let chars: Vec<_> = repeat(1.., none_of('\n')).parse_next(input)?;
+    Ok(chars
+        .into_iter()
+        .map(|c| c.to_digit(10).unwrap() as u8)
+        .collect())
+}
+
+fn dfs_reach(start: &Point, grid: &Grid) {}
+
+impl Day for Day10 {
+    type Input = Puzzle;
+
+    fn parser(input: &mut &str) -> PResult<Self::Input> {
+        let elevations: Vec<_> = separated(1.., parse_row, line_ending).parse_next(input)?;
+        let mut trail_heads = Vec::new();
+        for (y, row) in elevations.iter().enumerate() {
+            for (x, elevation) in row.iter().enumerate() {
+                if *elevation == 0 {
+                    trail_heads.push(Point {
+                        x: x.try_into().unwrap(),
+                        y: y.try_into().unwrap(),
+                    });
+                }
+            }
+        }
+        Ok(Puzzle {
+            map: elevations,
+            trail_heads,
+        })
     }
 
     type Output1 = usize;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        println!("{input:?}");
+        0
     }
 
     type Output2 = usize;
