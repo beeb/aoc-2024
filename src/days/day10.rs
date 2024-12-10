9712e3fcc74d85eb364dc023a79c2f1670324437
beeb <703631+beeb@users.noreply.github.com>
2024-12-10 07:29:14 +0100

    feat(day10): part 1

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,4 +1,3 @@
-use pathfinding::grid::Grid;
 use winnow::{
     ascii::line_ending,
     combinator::{repeat, separated},
@@ -8,17 +7,36 @@ use winnow::{
 
 use crate::days::Day;
 
-const GRID_SIZE: usize = 48;
-
 /// Top - Right - Bottom - Left
-const DIRS: [(isize, isize); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+const DIRS: [(i8, i8); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
 
 pub struct Day10;
+
+pub type HashSet<T> = std::collections::HashSet<T, ahash::RandomState>;
 
 #[derive(Debug, PartialEq, Eq, Default, Clone, Hash)]
 pub struct Point {
     x: i8,
     y: i8,
+}
+
+impl Point {
+    fn elevation(&self, map: &[Vec<u8>]) -> u8 {
+        map[self.y as usize][self.x as usize]
+    }
+
+    fn neighbors(&self, map: &[Vec<u8>]) -> Vec<Point> {
+        let elev = self.elevation(map);
+        DIRS.iter()
+            .map(|(dx, dy)| Point {
+                x: self.x + dx,
+                y: self.y + dy,
+            })
+            .filter(|p| {
+                map.get(p.y as usize).and_then(|row| row.get(p.x as usize)) == Some(&(elev + 1))
+            })
+            .collect()
+    }
 }
 
 #[derive(Debug, Clone, Default)]
@@ -35,7 +53,18 @@ fn parse_row(input: &mut &str) -> PResult<Vec<u8>> {
         .collect())
 }
 
-fn dfs_reach(start: &Point, grid: &Grid) {}
+fn bfs_reach(start: &Point, map: &[Vec<u8>]) -> HashSet<Point> {
+    let mut goals = HashSet::<Point>::default();
+    let mut to_visit: Vec<Point> = start.neighbors(map);
+    while let Some(candidate) = to_visit.pop() {
+        if candidate.elevation(map) == 9 {
+            goals.insert(candidate);
+        } else {
+            to_visit.extend(candidate.neighbors(map).into_iter());
+        }
+    }
+    goals
+}
 
 impl Day for Day10 {
     type Input = Puzzle;
@@ -62,8 +91,11 @@ impl Day for Day10 {
     type Output1 = usize;
 
     fn part_1(input: &Self::Input) -> Self::Output1 {
-        println!("{input:?}");
-        0
+        input
+            .trail_heads
+            .iter()
+            .map(|p| bfs_reach(p, &input.map).len())
+            .sum()
     }
 
     type Output2 = usize;
