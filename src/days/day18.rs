7af07acd893a684ceb38188f035a6f4cadefbd28
beeb <703631+beeb@users.noreply.github.com>
2024-12-18 08:48:59 +0100

    feat: day 18

--- src/days/day18.rs
+++ src/days/day18.rs
@@ -1,25 +1,104 @@
-use winnow::PResult;
+use pathfinding::{grid::Grid, prelude::astar};
+use winnow::{
+    ascii::{dec_uint, line_ending},
+    combinator::separated,
+    seq, PResult, Parser as _,
+};
 
 use crate::days::Day;
 
+const GRID_SIZE: usize = if cfg!(test) { 7 } else { 71 };
+const PART1_LEN: usize = if cfg!(test) { 12 } else { 1024 };
+
 pub struct Day18;
 
-impl Day for Day18 {
-    type Input = String;
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct Pos {
+    x: usize,
+    y: usize,
+}
 
-    fn parser(_input: &mut &str) -> PResult<Self::Input> {
-        unimplemented!("parser")
+impl Pos {
+    fn distance(&self, other: &Pos) -> usize {
+        self.x.abs_diff(other.x) + self.y.abs_diff(other.y)
+    }
+
+    fn successors(&self, grid: &Grid) -> Vec<(Pos, usize)> {
+        grid.neighbours(self.into())
+            .into_iter()
+            .map(|p| (p.into(), 1))
+            .collect()
+    }
+}
+
+impl From<&Pos> for (usize, usize) {
+    fn from(value: &Pos) -> Self {
+        (value.x, value.y)
+    }
+}
+
+impl From<(usize, usize)> for Pos {
+    fn from(value: (usize, usize)) -> Self {
+        Self {
+            x: value.0,
+            y: value.1,
+        }
+    }
+}
+
+fn parse_pos(input: &mut &str) -> PResult<Pos> {
+    seq!(Pos {
+        x: dec_uint,
+        _: ',',
+        y: dec_uint
+    })
+    .parse_next(input)
+}
+
+fn make_grid(obstacles: &[Pos]) -> Grid {
+    let mut grid = Grid::new(GRID_SIZE, GRID_SIZE);
+    grid.fill();
+    for obs in obstacles {
+        grid.remove_vertex(obs.into());
+    }
+    grid
+}
+
+impl Day for Day18 {
+    type Input = Vec<Pos>;
+
+    fn parser(input: &mut &str) -> PResult<Self::Input> {
+        separated(1.., parse_pos, line_ending).parse_next(input)
     }
 
     type Output1 = usize;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        let goal: Pos = (GRID_SIZE - 1, GRID_SIZE - 1).into();
+        let grid = make_grid(input.get(0..PART1_LEN).unwrap());
+        let (_, score) = astar(
+            &Pos { x: 0, y: 0 },
+            |p| p.successors(&grid),
+            |p| p.distance(&goal),
+            |p| *p == goal,
+        )
+        .unwrap();
+        score
     }
 
-    type Output2 = usize;
+    type Output2 = String;
 
-    fn part_2(_input: &Self::Input) -> Self::Output2 {
-        unimplemented!("part_2")
+    fn part_2(input: &Self::Input) -> Self::Output2 {
+        let start = (0, 0);
+        let goal = (GRID_SIZE - 1, GRID_SIZE - 1);
+        let (first, second) = input.split_at(PART1_LEN);
+        let mut grid = make_grid(first);
+        for obs in second {
+            grid.remove_vertex(obs.into());
+            if !grid.dfs_reachable(start, |_| true).contains(&goal) {
+                return format!("{},{}", obs.x, obs.y);
+            }
+        }
+        "".to_string()
     }
 }
